96ccb28079fc7ebbaea78dc3361bd0c8f70eb2eb
Randhir <randhir.kumar@knoldus.in>
2018-03-05 10:18:05 +0530

    assigment done

--- src/main/java/edu/knoldus/Application.java
+++ src/main/java/edu/knoldus/Application.java
@@ -7,7 +7,7 @@ import java.util.stream.IntStream;
 public class Application {
 	public static void listMultiply(List<Integer> list1, List<Integer> list2) {
 		List<Integer> result = new ArrayList<>();
-		IntStream.range(0, list1.size()).forEach(index -> result.add(list1.get(index) * list2.get(index)));
+		IntStream.range(0, Math.min(list1.size(), list2.size())).forEach(index -> result.add(list1.get(index) * list2.get(index)));
 		System.out.println(result);
 	}
 
@@ -18,6 +18,10 @@ public class Application {
 		String stringData = "My name is randhir My name is randhir";
 		List<String> stringList = Arrays.asList(stringData.split("\\s+"));
 		Map<String, Integer> wordCountMap = stringList.stream().collect(Collectors.toMap(word -> word, word -> 1, Integer::sum));
-		System.out.println(wordCountMap);
+		List<List<Object>> wordCountList = new LinkedList<>();
+		wordCountMap.forEach((key, value) -> {
+			wordCountList.add(Arrays.asList(key, value));
+		});
+		System.out.println(wordCountList);
 	}
 }
